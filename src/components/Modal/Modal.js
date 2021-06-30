5a7842ff7081646468f60944698f553def356b1f
John Hall <johngradyh@gmail.com>
2021-06-30 16:26:38 -0400

    animated list

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -15,7 +15,12 @@ const modal = (props) => {
       timeout={animationTiming}
       mountOnEnter
       unmountOnExit
-      classNames="fade-slide"
+      classNames={{
+          enter: "",
+          enterActive: "ModalOpen",
+          exit: "",
+          exitActive: "ModalClosed",
+      }}
     >
       <div className="Modal">
         <h1>A Modal</h1>
